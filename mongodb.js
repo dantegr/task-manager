8bc44069c0882bbb02e3ac2bb8421c4f5c7abac2
Pantelis <tampantelis@gmail.com>
2019-05-28 21:01:10 +0300

    deleting documents

--- mongodb.js
+++ mongodb.js
@@ -26,16 +26,23 @@ MongoClient.connect(connectionURL, { useNewUrlParser:true }, (error,client) => {
   //   console.log(error)
   // });
   
-  db.collection('tasks').updateMany({
-    completed: false
-  },{
-    $set: {
-      completed: true
-    }
-  }).then((result) => {
-    console.log(result);
-  }).catch((error) => {
-    console.log(error)
-  });
+  // db.collection('tasks').updateMany({
+  //   completed: false
+  // },{
+  //   $set: {
+  //     completed: true
+  //   }
+  // }).then((result) => {
+  //   console.log(result);
+  // }).catch((error) => {
+  //   console.log(error)
+  // });
 
+  db.collection('users').deleteMany({
+    age: 32
+  }).then((result) => {
+      console.log(result);
+    }).catch((error) => {
+      console.log(error)
+    });
 });
