c408cff6b78c41673b43f21980d7d42000db3596
Pantelis <tampantelis@gmail.com>
2019-05-28 20:55:01 +0300

    udating documents

--- mongodb.js
+++ mongodb.js
@@ -13,20 +13,29 @@ MongoClient.connect(connectionURL, { useNewUrlParser:true }, (error,client) => {
   }
 
   const db = client.db(databaseName);
-
-  db.collection('users').find({age: 32}).toArray((error,user) => {
-    if (error) {
-      return console.log('Unable to fetch');
-     }
-
-     console.log(user);
+  
+  // db.collection('users').updateOne({
+  //   _id :new ObjectID("5ce1851e0aa1143880832a63")
+  // },{
+  //   $inc: {
+  //     age: 5
+  //   }
+  // }).then((result) => {
+  //   console.log(result);
+  // }).catch((error) => {
+  //   console.log(error)
+  // });
+  
+  db.collection('tasks').updateMany({
+    completed: false
+  },{
+    $set: {
+      completed: true
+    }
+  }).then((result) => {
+    console.log(result);
+  }).catch((error) => {
+    console.log(error)
   });
 
-  db.collection('users').find({age: 32}).count((error,user) => {
-    if (error) {
-      return console.log('Unable to fetch');
-     }
-
-     console.log(user);
-  });
 });
